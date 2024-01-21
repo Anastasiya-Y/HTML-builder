070eb6915d689d27a704970d3f433162d90757fa
Anastasiya Yacheykina <yacheykina_nastya@mail.ru>
2024-01-21 21:27:23 +0300

    feat: add 05-merge-styles task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,38 @@
+const path = require('node:path');
+const fsPromises = require('node:fs/promises');
+const { exit } = require('node:process');
+
+const projectDistDir = path.join(__dirname, 'project-dist');
+const stylesDir = path.join(__dirname, 'styles');
+const bundle = path.join(projectDistDir, 'bundle.css');
+
+const mergeStyles = async () => {
+  try {
+    const fileCreation = await fsPromises.open(bundle, 'w');
+    await fileCreation.close();
+
+    const stylesArr = [];
+
+    const files = await fsPromises.readdir(stylesDir, { withFileTypes: true });
+
+    for (const file of files) {
+      if (file.isFile()) {
+        const fileTarget = path.join(stylesDir, file.name);
+
+        if (path.extname(fileTarget).toLowerCase() === '.css') {
+          const style = await fsPromises.readFile(fileTarget, 'utf-8');
+          stylesArr.push(style, '\n');
+        }
+      }
+    }
+
+    for (const chunk of stylesArr) {
+      await fsPromises.appendFile(bundle, chunk);
+    }
+    exit();
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+mergeStyles();
